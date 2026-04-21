ef365cb35bbb18d9753ac61c0fe4743ec3e7ad49
Jan Torres <153082161+JanToro26@users.noreply.github.com>
2026-04-21 18:32:44 +0200

    Using random values

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,26 @@
 package com.example;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] table = new int[3][3];
+        Random rand = new Random(); // Create random generator
+
+        // Fill with random numbers between 0 and 100
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                table[i][j] = rand.nextInt(101); 
+            }
+        }
+
+        // Print the table
+        System.out.println("Java Table:");
+        for (int[] row : table) {
+            for (int element : row) {
+                System.out.print(element + "\t");
+            }
+            System.out.println();
+        }
     }
 }
